96cfd7090e6a7785c024dd7c7514996d497f36fd
Peter Zimmerman <pzim10@gmail.com>
2015-06-30 20:09:27 -0600

    Window

--- PersonList/AppDelegate.m
+++ PersonList/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:self.window.frame];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
